d4a603cd225cab6cde87ff14d3cb28c5a19f906a
RobsonBSI <103039603+RobsonBSI@users.noreply.github.com>
2023-03-29 21:00:52 -0300

    atualização

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Cliente {
+public class Cliente implements Cloneable{
+    private int id;
+    private String nome;
+    private Endereco endereco;
+    private String telefone;
+    public Cliente(int id, String nome, Endereco endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone= telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    @Override
+    public Cliente clone() throws CloneNotSupportedException {
+        Cliente clienteClone = (Cliente) super.clone();
+        clienteClone.endereco = (Endereco) clienteClone.endereco.clone();
+        return clienteClone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ",nome='" + nome + '\'' +
+                ",endereco=" + endereco +
+                ",telefone='" + telefone + '\'' +
+                '}';
+    }
 }
--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -1,4 +1,20 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ClienteTest {
-  
+    @Test
+    void testClone() throws CloneNotSupportedException {
+        Cliente cliente = new Cliente(123, "cliente Original", new Endereco("Rua A", 1), "(32)98994-9999");
+
+       Cliente clienteclone = cliente.clone();
+        clienteclone.setId(124);
+        clienteclone.setNome("cliente Clonado");
+        clienteclone.getEndereco().setNumero(24);
+
+       clienteclone.setTelefone("(32)98995-9988");
+
+        assertEquals("Cliente{id=123,nome='cliente Original',endereco=Endereco{logradouro='Rua A', numero=1},telefone='(32)98994-9999'}", cliente.toString());
+        assertEquals("Cliente{id=124,nome='cliente Clonado',endereco=Endereco{logradouro='Rua A', numero=24},telefone='(32)98995-9988'}", clienteclone.toString());
+    }
 }
